8f4b7d7292e5b2e083f8f1bf5cbbc597fd1356a9
Yuval Kogman <nothingmuch@woobling.org>
2009-05-21 14:10:24 +0200

    Add a trailing newline to the HTML output

--- make_pre_tag.pl
+++ make_pre_tag.pl
@@ -11,5 +11,10 @@ my $gist = shift or die "No gist ID specified";
 my $raw = get("http://gist.github.com/${gist}.txt");
 my $escaped = encode_entities($raw);
 
-print qq{<pre id="fake-gist-$gist" class="fake-gist">\n$escaped</pre>\n};
+chomp $escaped; # we add a newline unconditionally below
 
+print <<HTML;
+<pre id="fake-gist-$gist" class="fake-gist">
+$escaped
+</pre>
+HTML
